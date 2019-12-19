83902eb33f890e8237c5dc7d4112ec7e89dc721f
David Martín <davidmb246@gmail.com>
2019-12-19 18:27:22 +0100

    actu(la cruceta se habia perdido por error, nuevo personaje, ejemplo de bola de daño, nuevo personaje

--- Assets/Scripts/FPS/CharacterMeleDamage.cs
+++ Assets/Scripts/FPS/CharacterMeleDamage.cs
@@ -3,7 +3,7 @@
 public class CharacterMeleDamage : MonoBehaviour {
 
     public int damage;    //Damage the player will receive when enter the trigger
-    public string targetDamageTag;
+    public string targetDamageTag = "FPSController";
 
     private void OnTriggerStay(Collider other) {
         if (other.gameObject.CompareTag(targetDamageTag)) {
